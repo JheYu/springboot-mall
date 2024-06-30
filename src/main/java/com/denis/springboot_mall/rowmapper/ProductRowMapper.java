e92cbf6571717bc47eca4b52545bf07682e353b4
JHE-YU, Li <denis850628@gmail.com>
2024-06-30 23:01:18 +0800

    商品功能 - Refactor category，使用Enum類型

--- src/main/java/com/denis/springboot_mall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/denis/springboot_mall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.denis.springboot_mall.rowmapper;
 
+import com.denis.springboot_mall.constant.ProductCategory;
 import com.denis.springboot_mall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,13 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId((resultSet.getInt("product_id")));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+
+
         product.setImage_url(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
