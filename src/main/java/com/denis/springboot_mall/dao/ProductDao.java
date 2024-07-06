3b3b80cca33bc1e21cf4e2b941601e27609b99fd
JHE-YU, Li <denis850628@gmail.com>
2024-07-06 12:25:35 +0800

    商品功能 - 實作查詢商品列表（基礎實作)

--- src/main/java/com/denis/springboot_mall/dao/ProductDao.java
+++ src/main/java/com/denis/springboot_mall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.denis.springboot_mall.dao;
 import com.denis.springboot_mall.dto.ProductRequest;
 import com.denis.springboot_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
@@ -12,4 +16,6 @@ public interface ProductDao {
     void updateProduct(Integer productId, ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
+
+
 }
--- src/main/java/com/denis/springboot_mall/service/ProductService.java
+++ src/main/java/com/denis/springboot_mall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.denis.springboot_mall.service;
 import com.denis.springboot_mall.dto.ProductRequest;
 import com.denis.springboot_mall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
