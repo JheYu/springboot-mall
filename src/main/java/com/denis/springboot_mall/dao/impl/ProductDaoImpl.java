7d1bb19c0dc29288dadbf2a869fe7c959b3c6d2c
JHE-YU, Li <denis850628@gmail.com>
2024-07-06 13:51:30 +0800

    商品功能 - 實作查詢商品列表（分頁）

--- src/main/java/com/denis/springboot_mall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/denis/springboot_mall/dao/impl/ProductDaoImpl.java
@@ -32,6 +32,7 @@ public class ProductDaoImpl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        //查詢條件
         if(productQueryParams.getCategory() != null){
             sql = sql + " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
@@ -40,8 +41,14 @@ public class ProductDaoImpl implements ProductDao {
             sql = sql + " AND product_name LIKE :search";
                     map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
+
         //排序只能使用字串拼接的方式
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        //分頁
+        sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
