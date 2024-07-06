914d7c6d7d968ef6627d4154e81fdc49153601f0
JHE-YU, Li <denis850628@gmail.com>
2024-07-06 21:48:55 +0800

    帳號功能 - 註冊新帳號

---
+++ src/main/java/com/denis/springboot_mall/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.denis.springboot_mall.dao;
+
+import com.denis.springboot_mall.dto.UserRegisterRequest;
+import com.denis.springboot_mall.model.User;
+
+public interface UserDao {
+    Integer createUser(UserRegisterRequest userRegisterRequest);
+
+    User getUserById(Integer userId);
+}
---
+++ src/main/java/com/denis/springboot_mall/service/UserService.java
@@ -0,0 +1,10 @@
+package com.denis.springboot_mall.service;
+
+import com.denis.springboot_mall.dto.UserRegisterRequest;
+import com.denis.springboot_mall.model.User;
+
+public interface UserService {
+    Integer register(UserRegisterRequest userRegisterRequest);
+
+    User getUserById(Integer userId);
+}
